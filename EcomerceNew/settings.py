3ba599ad216159bbb2ab4edbdc95348c12655bbb
Mehedi Hasan <43444174+mehedihasan555552@users.noreply.github.com>
2021-07-17 02:16:59 +0600

    Update settings.py

--- EcomerceNew/settings.py
+++ EcomerceNew/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'gg+^wh0@42gnyb3*^=218i(m)or*cff=@#shw7@hm8&^n04m3r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
