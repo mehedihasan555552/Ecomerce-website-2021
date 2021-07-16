a259e0d83980d252ff4b50b1db6375dfbba66534
Mehedi Hasan <43444174+mehedihasan555552@users.noreply.github.com>
2021-07-17 02:20:34 +0600

    Update settings.py

--- EcomerceNew/settings.py
+++ EcomerceNew/settings.py
@@ -137,8 +137,8 @@ MEDIA_ROOT= (BASE_DIR / 'static/images')
 # SMTP configuration
 # EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT = config('EMAIL_PORT', cast=int)
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
